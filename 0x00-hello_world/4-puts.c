435fafbd7233bece1d588e4f80887405674b23b4
Dywili <leroydywili7@gmail.com>
2023-02-16 12:38:32 -0800

    4-puts.c_lastcorrection

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 /**
  * main - this is the main function
+ *
  * Return: always 0
  */
 
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
