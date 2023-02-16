b9df60edbe2dc7fb8c876e218b1d9ee87935b481
Dywili <leroydywili7@gmail.com>
2023-02-16 12:32:38 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - this is the main function
- *
  * Return: always 0
  */
 
- int main(void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	put("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
